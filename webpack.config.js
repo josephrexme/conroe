760ab5b02d1321ac76a89b7e61ee1b57b0e15d61
Joseph Rex <joerexko@gmail.com>
2018-12-03 14:34:12 -0600

    Remove base tag

--- webpack.config.js
+++ webpack.config.js
@@ -91,7 +91,7 @@ const config = {
       hash: true,
       mobile: true,
       favicon: 'public/favicon.ico',
-      headHtmlSnippet: '<base href="/" /><link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Open+Sans+Condensed:700" rel="stylesheet">',
+      headHtmlSnippet: '<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Open+Sans+Condensed:700" rel="stylesheet">',
       appMountId: 'app',
     }),
     new webpack.EnvironmentPlugin(['NODE_ENV', 'HOST']),
